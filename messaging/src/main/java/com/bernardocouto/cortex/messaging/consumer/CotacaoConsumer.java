1cfefb4bcceb8bdc71a63251535996bee18ae1e5
Bernardo Couto <bernardocouto@icloud.com>
2020-10-17 10:41:16 -0300

    Configuração da parte de mensageria com AWS SQS

---
+++ messaging/src/main/java/com/bernardocouto/cortex/messaging/consumer/CotacaoConsumer.java
@@ -0,0 +1,14 @@
+package com.bernardocouto.cortex.messaging.consumer;
+
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CotacaoConsumer {
+
+    @SqsListener(deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS, value = "${aws.sqs.queue.name}")
+    public void receiveMessage(@Payload final String message) { }
+
+}
